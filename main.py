0d98f01909e7eea0934db78b5fc51b35d2a4114c
Maria Amasialidou <79280783+MariaAma@users.noreply.github.com>
2024-04-15 04:59:01 +0300

    main.py

---
+++ main.py
@@ -0,0 +1,55 @@
+import spotipy
+from spotipy.oauth2 import SpotifyOAuth
+import streamlit as st
+import os 
+import pandas as pd
+from dotenv import load_dotenv
+
+
+#----------
+track_ids = []
+track_name= []
+
+
+#----------
+#Take environment variables from .env file and scope
+load_dotenv()  
+client_id = os.getenv("Client_ID")
+client_secret = os.getenv("Client_secret")
+redirect_uri= os.getenv("redirect_uri")
+
+
+#----------
+sp = spotipy.Spotify(
+                    auth_manager=SpotifyOAuth(client_id= client_id,
+                                            client_secret=client_secret,
+                                            redirect_uri= redirect_uri,
+                                            scope='user-read-recently-played')
+                    )
+
+st.set_page_config(page_title="Spotify Extract&Analysis Project", page_icon="🎵")
+st.title('Spotify API Project')
+base="dark"
+
+
+
+#----------
+top_tracks = sp.current_user_recently_played(limit=50)
+for idx, item in enumerate(top_tracks['items']):
+    track = item['track']
+    print(idx, track['artists'][0]['name'], " - ", track['name'], "-", track ['id'])
+    track_ids.append(track['id'])
+    track_name.append(track['name'])
+
+
+#----------
+audio_features = sp.audio_features(track_ids)
+df = pd.DataFrame(audio_features)
+df['track_name'] = track_name
+df = df[['track_name', 'danceability', 'energy', 'valence']]
+df.set_index('track_name', inplace = True)
+
+#----------
+st.subheader('Audio Features for my Top 10 tracks')
+st.bar_chart(df, height= 500)
+
